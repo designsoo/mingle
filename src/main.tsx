545b4da4d79714adbd56e7167609123df55f7f8f
designsoo <kheesoo10@gmail.com>
2024-06-18 00:44:28 +0900

    feat: plus jakarta sans 폰트 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { createRoot } from 'react-dom/client';
+import '@fontsource/plus-jakarta-sans/400.css';
+import '@fontsource/plus-jakarta-sans/600.css';
 
 import App from './App.tsx';
 import './index.css';
