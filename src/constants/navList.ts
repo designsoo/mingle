787edff84e60718c4fc28ebe6d431ccc063676f3
designsoo <kheesoo10@gmail.com>
2024-06-18 00:48:24 +0900

    feat: menu 리스트 상수화

---
+++ src/constants/navList.ts
@@ -0,0 +1,8 @@
+import { SVGS } from '@/constants';
+const { home, search, dashboard } = SVGS;
+
+export const NAV_LIST = [
+  { id: 0, value: home, path: '/' },
+  { id: 1, value: search, path: '/list' },
+  { id: 2, value: dashboard, path: '/post' },
+];
