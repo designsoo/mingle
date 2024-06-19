88d2d31b7a1d25cbe968faa4e47b1f46aec8ee36
designsoo <kheesoo10@gmail.com>
2024-06-19 22:47:48 +0900

    feat: 색상값을 이미지 URL로 변경하는 객체 생성

---
+++ src/constants/formatColor.ts
@@ -0,0 +1,8 @@
+import { CLOUDFLARE_URL, PREVIEW_PAPER_SIZE } from '@/constants';
+
+export const formatColorToUrl = {
+  blue: `${CLOUDFLARE_URL}/8a482541-9f00-429f-cff7-2b90fc4d0400/${PREVIEW_PAPER_SIZE}`,
+  purple: `${CLOUDFLARE_URL}/c69924f8-98e5-45bc-ccae-065cce04a500/${PREVIEW_PAPER_SIZE}`,
+  green: `${CLOUDFLARE_URL}/9faebeef-e89a-40ec-5a23-703d32e95000/${PREVIEW_PAPER_SIZE}`,
+  beige: `${CLOUDFLARE_URL}/dc6b7cff-ba50-4674-dff4-e092f38a6700/${PREVIEW_PAPER_SIZE}`,
+};
