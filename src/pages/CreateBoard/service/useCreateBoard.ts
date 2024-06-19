8e69da1c8348d6a1933e803d2080d6308989a3bd
designsoo <kheesoo10@gmail.com>
2024-06-19 22:54:42 +0900

    feat: useMutation을 통한 useCreateBoard 함수 구현

---
+++ src/pages/CreateBoard/service/useCreateBoard.ts
@@ -0,0 +1,19 @@
+import { useMutation } from '@tanstack/react-query';
+import { NavigateFunction } from 'react-router-dom';
+
+import { RECIPIENTS } from '@/api/recipients';
+
+export const useCreateBoard = (navigate: NavigateFunction) => {
+  const { mutate: postFormMutation } = useMutation({
+    mutationFn: RECIPIENTS.post,
+    onSuccess: (response) => {
+      const newBoardUrl = response.data.id;
+      navigate(`/recipients/${newBoardUrl}`);
+    },
+    onError: () => {
+      alert('대시보드 생성에 실패했습니다. 다시 시도해 주세요.');
+    },
+  });
+
+  return { postFormMutation };
+};
