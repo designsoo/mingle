31c31e5b9ac6b13c077003c6ca8ea7ce8c58722c
designsoo <kheesoo10@gmail.com>
2024-06-20 17:13:41 +0900

    feat: tab, sort 리스트 상수화

--- src/constants/tabList.ts
+++ src/constants/tabList.ts
@@ -2,3 +2,16 @@ export const TAB_LIST = [
   { id: 0, value: 'Color' },
   { id: 1, value: 'Image' },
 ];
+
+export const AUTHOR_LIST = [
+  { id: 'all', value: 'All' },
+  { id: '가족', value: 'Family' },
+  { id: '친구', value: 'Friends' },
+  { id: '동료', value: 'Colleagues' },
+  { id: '지인', value: 'Acquaintances' },
+];
+
+export const SORT_OPTIONS = [
+  { id: 'desc', value: 'Latest' },
+  { id: 'asc', value: 'Oldest' },
+];
