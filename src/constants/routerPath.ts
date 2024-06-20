b29f9ba92264c274b89116841798c759a3e17fd7
designsoo <kheesoo10@gmail.com>
2024-06-20 17:13:16 +0900

    feat: router 경로 추가 - detail, edit, create messages

--- src/constants/routerPath.ts
+++ src/constants/routerPath.ts
@@ -2,4 +2,7 @@ export const ROUTER_PATH = {
   landing: '/',
   boardList: '/list',
   createBoard: '/post',
+  detailBoard: '/board/:id',
+  editBoard: '/board/:id/edit',
+  createMessage: '/post/:id/message',
 };
