abcee591957960b5fea544780103704c3207426e
designsoo <kheesoo10@gmail.com>
2024-06-20 17:16:59 +0900

    feat: 보드 상세보기 페이지 router 추가

--- src/MyRouter.tsx
+++ src/MyRouter.tsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ROUTER_PATH } from '@/constants';
 
 import CreateBoard from '@/pages/CreateBoard';
+import DetailBoard from '@/pages/DetailBoard';
 
-const { createBoard } = ROUTER_PATH;
+const { createBoard, detailBoard } = ROUTER_PATH;
 
 const MyRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path={createBoard} element={<CreateBoard />} />
+        <Route path={detailBoard} element={<DetailBoard />} />
       </Routes>
     </Router>
   );
