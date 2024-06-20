10b61bd21479de6c9cee73afea83052de33ac2ec
designsoo <kheesoo10@gmail.com>
2024-06-20 17:10:05 +0900

    feat: getMessages api 추상화 추가

--- src/api/recipients.ts
+++ src/api/recipients.ts
@@ -1,10 +1,11 @@
-import { RECIPIENTS_API } from '@/constants';
+import { RECIPIENTS_API, MESSAGE_API } from '@/constants';
 
 import instance from './axiosInstance';
 
 export const RECIPIENTS = {
   getBoardList: () => instance.get(RECIPIENTS_API),
   getBoard: (recipientId: number) => instance.get(`${RECIPIENTS_API}${recipientId}/`),
+  getMessages: (recipientId: number) => instance.get(`${RECIPIENTS_API}${recipientId}${MESSAGE_API}`),
   post: (data: object) => instance.post(RECIPIENTS_API, data),
   delete: (recipientId: number) => instance.delete(`${RECIPIENTS_API}${recipientId}/`),
 };
