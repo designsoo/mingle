924ce32837f9a164f1e15fc94e91574c30e95abd
designsoo <kheesoo10@gmail.com>
2024-06-23 00:18:11 +0900

    feat: Board Count 컴포넌트 분리 및 구현

---
+++ src/components/pages/DetailBoard/BoardCount.tsx
@@ -0,0 +1,25 @@
+type BoardCountProps = {
+  paperCount: number;
+  reactionCount: number;
+};
+
+const BoardCount = ({ paperCount, reactionCount }: BoardCountProps) => {
+  return (
+    <ul className='board-count flex gap-6'>
+      <li className='flex items-center gap-2'>
+        <span className='text-base-14 text-neutral-500'>Paper</span>
+        <span className='text-bold-18'>{paperCount}</span>
+      </li>
+      <li className='flex items-center gap-2'>
+        <span className='text-base-14 text-neutral-500'>Members</span>
+        <span className='text-bold-18'>{paperCount}</span>
+      </li>
+      <li className='flex items-center gap-2'>
+        <span className='text-base-14 text-neutral-500'>Emotions</span>
+        <span className='text-bold-18'>{reactionCount}</span>
+      </li>
+    </ul>
+  );
+};
+
+export default BoardCount;
