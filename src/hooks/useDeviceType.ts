d8c8549427f91906fe86d324aebba7b3c9b4d26f
designsoo <kheesoo10@gmail.com>
2024-06-26 01:08:35 +0900

    feat: window 너비에 따른 디바이스 타입 반환하는 훅 구현

---
+++ src/hooks/useDeviceType.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+import { BREAK_POINT } from '@/constants';
+
+import { DeviceType } from '@/types/deviceType';
+
+export const useDeviceType = (): DeviceType => {
+  const detectDeviceType = (): DeviceType => {
+    const windowWidth = typeof window !== 'undefined' ? window.innerWidth : 0;
+
+    if (windowWidth <= BREAK_POINT.mobile) {
+      return DeviceType.sm;
+    }
+
+    if (windowWidth <= BREAK_POINT.tablet) {
+      return DeviceType.md;
+    }
+
+    if (windowWidth <= BREAK_POINT.pc) {
+      return DeviceType.lg;
+    }
+    return DeviceType.xl;
+  };
+
+  const [currentDeviceType, setCurrentDeviceType] = useState<DeviceType>(DeviceType.xl);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newDeviceType = detectDeviceType();
+      setCurrentDeviceType(newDeviceType);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return currentDeviceType;
+};
