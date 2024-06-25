bf7b507818db264282075320e7fdc75728d7c55a
designsoo <kheesoo10@gmail.com>
2024-06-26 01:06:07 +0900

    refactor: 매직넘버 상수화

--- src/utils/getReactionsByDeviceType.ts
+++ src/utils/getReactionsByDeviceType.ts
@@ -1,8 +1,11 @@
 import { DeviceType } from '@/types/deviceType';
 import { EmojiResults } from '@/types/recipients';
 
+const TOP_REACTIONS = 3;
+const MAX_REACRIONS = 8;
+
 export const getReactionsByDeviceType = (deviceType: DeviceType, reactionData: EmojiResults[]) => {
-  const topRection = reactionData?.slice(0, 3);
-  const maxReaction = reactionData?.slice(0, 8);
+  const topRection = reactionData?.slice(0, TOP_REACTIONS);
+  const maxReaction = reactionData?.slice(0, MAX_REACRIONS);
   return deviceType !== 'Mobile' ? topRection : maxReaction;
 };
