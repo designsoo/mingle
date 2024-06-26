8df6a482113aa0abfa7537d371eb550a60a9283d
designsoo <kheesoo10@gmail.com>
2024-06-27 08:15:46 +0900

    refactor: 목적에 맞게 api 분리 및 추상화, 추후 파일 분리

--- src/api/recipients.ts
+++ src/api/recipients.ts
@@ -7,10 +7,19 @@ import instance from './axiosInstance';
 export const RECIPIENTS = {
   getBoardList: () => instance.get(RECIPIENTS_API),
   getBoard: (recipientId: number) => instance.get(`${RECIPIENTS_API}${recipientId}/`),
-  getMessages: (recipientId: number) => instance.get(`${RECIPIENTS_API}${recipientId}${MESSAGE_API}`),
   post: (data: object) => instance.post(RECIPIENTS_API, data),
-  delete: (recipientId: number) => instance.delete(`${RECIPIENTS_API}${recipientId}/`),
+  deleteBoard: (recipientId: number) => instance.delete(`${RECIPIENTS_API}${recipientId}/`),
+  getMessages: (recipientId: number) => instance.get(`${RECIPIENTS_API}${recipientId}${MESSAGE_API}`),
   createReaction: ({ boardId, emojiForm }: { boardId: number; emojiForm: PostEmoji }) =>
     instance.post(`${RECIPIENTS_API}${boardId}/reactions/`, emojiForm),
   getReaction: (recipientId: number) => instance.get(`${RECIPIENTS_API}${recipientId}/reactions/`),
+};
+
+export const MESSAGES = {
+  get: (recipientId: number) => instance.get(`${RECIPIENTS_API}${recipientId}${MESSAGE_API}`),
+  delete: (messageId: number) => instance.delete(`${MESSAGE_API}${messageId}/`),
+};
+
+export const REACTIONS = {
+  get: (recipientId: number) => instance.get(`${RECIPIENTS_API}${recipientId}/reactions/`),
 };
