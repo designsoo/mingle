fca7355259b46982c5b3f7c7ff7b996a0f72c48e
designsoo <kheesoo10@gmail.com>
2024-06-27 08:14:16 +0900

    feat: react-modal의 앱 요소를 '#root'로 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import '@fontsource/plus-jakarta-sans/400.css';
 import '@fontsource/plus-jakarta-sans/600.css';
+import Modal from 'react-modal';
 
 import App from './App.tsx';
 import './index.css';
+
+Modal.setAppElement('#root');
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
