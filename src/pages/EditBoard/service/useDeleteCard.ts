12c8be6e1e899297b90c9a2f7c9c74067f4197ed
designsoo <kheesoo10@gmail.com>
2024-06-27 08:27:33 +0900

    feat: 카드 삭제하기 로직 구현

---
+++ src/pages/EditBoard/service/useDeleteCard.ts
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { MESSAGES } from '@/api/recipients';
+
+export const useDeleteCard = (boardId: number) => {
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteMessageMutation } = useMutation({
+    mutationFn: MESSAGES.delete,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['messages', boardId] });
+      queryClient.invalidateQueries({ queryKey: ['recipient', boardId] });
+    },
+    onError: () => {
+      alert('페이퍼 삭제에 실패했습니다. 다시 시도해 주세요.');
+    },
+  });
+
+  return { deleteMessageMutation };
+};
