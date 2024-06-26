97d4126e53ab7e7a30ba4c7af5196c6f32edf823
designsoo <kheesoo10@gmail.com>
2024-06-27 08:09:32 +0900

    chore: eslint import 순서 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -15,6 +19,9 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'react', 'react-hooks', 'import'],
   settings: {
+    react: {
+      version: 'detect',
+    },
     'import/resolver': {
       typescript: {
         alwaysTryTypes: true,
@@ -38,16 +45,16 @@ module.exports = {
         pathGroups: [
           { pattern: 'react', group: 'external', position: 'before' },
           { pattern: 'react/**', group: 'external', position: 'before' },
-          { pattern: '@/pages/**', group: 'unknown' },
           { pattern: '@/api/**', group: 'unknown' },
           { pattern: '@/components/**', group: 'unknown' },
           { pattern: '@/types/**', group: 'unknown' },
           { pattern: '@/constants/**', group: 'unknown' },
-          { pattern: '@/utils/**', group: 'unknown' },
+          { pattern: '@/pages/**', group: 'unknown' },
           { pattern: '@/hooks/**', group: 'unknown' },
+          { pattern: '@/utils/**', group: 'unknown' },
           { pattern: './**', group: 'index' },
         ],
-        pathGroupsExcludedImportTypes: ['builtin'],
+        pathGroupsExcludedImportTypes: ['react'],
         'newlines-between': 'always',
         alphabetize: { order: 'asc', caseInsensitive: true },
       },
