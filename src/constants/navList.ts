44bb072f12d5ba6497427df8f655564a8e56d6af
designsoo <kheesoo10@gmail.com>
2024-06-27 08:21:40 +0900

    feat: router path 상수 적용

--- src/constants/navList.ts
+++ src/constants/navList.ts
@@ -1,8 +1,10 @@
-import { SVGS } from '@/constants';
+import { ROUTER_PATH, SVGS } from '@/constants';
+
 const { home, search, dashboard } = SVGS;
+const { landing, boardList, createBoard } = ROUTER_PATH;
 
 export const NAV_LIST = [
-  { id: 0, value: home, path: '/' },
-  { id: 1, value: search, path: '/list' },
-  { id: 2, value: dashboard, path: '/post' },
+  { id: 0, value: home, path: landing },
+  { id: 1, value: search, path: boardList },
+  { id: 2, value: dashboard, path: createBoard },
 ];
