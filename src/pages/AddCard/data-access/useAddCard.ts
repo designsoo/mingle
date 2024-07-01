737a655a38a13e30a5ac0bd0f78fd5b9a702b4e3
designsoo <kheesoo10@gmail.com>
2024-07-01 22:52:57 +0900

    feat: 카드 생성 기능을 위한 useAddCard 훅 추가

---
+++ src/pages/AddCard/data-access/useAddCard.ts
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+
+import { MESSAGES } from '@/api/recipients';
+
+export const useAddCard = (boardId: number) => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { mutate: postCardMutation, isPending: isPostCardLoading } = useMutation({
+    mutationFn: MESSAGES.create,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['recipient', boardId] });
+      queryClient.invalidateQueries({ queryKey: ['messages', boardId] });
+      navigate(`/board/${boardId}`);
+    },
+    onError: () => {
+      alert('카드 생성에 실패했습니다. 다시 시도해 주세요.');
+    },
+  });
+
+  return { postCardMutation, isPostCardLoading };
+};
