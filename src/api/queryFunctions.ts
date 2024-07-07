7920a7ebd1b0971f735bc862a3d56c483e3a3404
designsoo <kheesoo10@gmail.com>
2024-07-08 05:44:32 +0900

    feat: 보드 리스트를 가져오는 queryFunction 선언, queryKey를 params prop으로 전달

--- src/api/queryFunctions.ts
+++ src/api/queryFunctions.ts
@@ -1,5 +1,15 @@
 import { MESSAGES, RECIPIENTS } from './recipients';
 
+export const getBoardList = async ({
+  queryKey,
+}: {
+  queryKey: [string, (string | undefined)?, (number | undefined)?];
+}) => {
+  const [, sort, limit] = queryKey;
+  const response = await RECIPIENTS.getBoardList(sort, limit);
+  return response.data.results;
+};
+
 export const getBoard = async (boardId: number) => {
   const response = await RECIPIENTS.getBoard(boardId);
   return response.data;
