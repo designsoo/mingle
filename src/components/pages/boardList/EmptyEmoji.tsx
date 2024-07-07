95898fd61edf895df10b04bd6a3da166293c5f21
designsoo <kheesoo10@gmail.com>
2024-07-08 05:38:56 +0900

    feat: EmptyEmoji 컴포넌트 구현

---
+++ src/components/pages/boardList/EmptyEmoji.tsx
@@ -0,0 +1,9 @@
+const EmptyEmoji = () => {
+  return (
+    <div className='flexbox-row-center w-full rounded-full py-2 color-background-opacity-white-10'>
+      <span className='text-bold-12'>No Emotions</span>
+    </div>
+  );
+};
+
+export default EmptyEmoji;
