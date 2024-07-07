acacde49fc7ebb98426f4ce34ebca03091890ed4
designsoo <kheesoo10@gmail.com>
2024-07-07 19:46:08 +0900

    refactor: board data 쿼리 로딩 상태 추가

--- src/pages/DetailBoard/data-access/useGetBoardData.ts
+++ src/pages/DetailBoard/data-access/useGetBoardData.ts
@@ -3,10 +3,10 @@ import { useQuery } from '@tanstack/react-query';
 import { getBoard } from '@/api/queryFunctions';
 
 export const useGetBoardData = (boardId: number) => {
-  const { data: boardData } = useQuery({
+  const { data: boardData, isLoading: isBoardDataLoading } = useQuery({
     queryKey: ['recipient', boardId],
     queryFn: () => getBoard(boardId),
   });
 
-  return { boardData };
+  return { boardData, isBoardDataLoading };
 };
