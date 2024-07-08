ec9ffe6b5e66c7dbdf2cc078fe67cd50a5917cd7
designsoo <kheesoo10@gmail.com>
2024-07-08 23:00:52 +0900

    refactor: useGetBoardList 훅을 사용하여 인기순, 최신순 데이터 추출, 검색기능 구현, 로딩중인 경우 스켈레톤UI 적용, 데이터 없는 경우 Empty UI 적용

--- src/pages/BoardList/index.tsx
+++ src/pages/BoardList/index.tsx
@@ -1,14 +1,16 @@
 import { useRef, useState } from 'react';
 
-import { useQuery } from '@tanstack/react-query';
-import { IconButton } from 'mingle-ui';
+import { EmptyBoardCard, IconButton } from 'mingle-ui';
 
 import { SVGS } from '@/constants';
 
-import { getBoardList } from '@/api/queryFunctions';
 import BoardCard from '@/components/pages/boardList/BoardCard';
+import BoardCardListSkeleton from '@/components/pages/boardList/BoardCardListSkeleton';
+import EmptyBoardCards from '@/components/pages/boardList/EmptyBoardCards';
 import Header from '@/components/ui/Header';
 import { BoardResults } from '@/types/recipients';
+
+import { useGetBoardList } from './data-access/useGetBoardList';
 
 const {
   arrow: { left, right },
@@ -18,18 +20,25 @@ const {
 const SCROLL_SLIDER_WIDRH = 1140;
 
 const BoardList = () => {
-  const { data: popularBoardData } = useQuery({
-    queryKey: ['recipients', 'like'],
-    queryFn: getBoardList,
-  });
-
-  const { data: latestBoardData } = useQuery({
-    queryKey: ['recipients', '', 99],
-    queryFn: getBoardList,
-  });
+  const { data: popularBoardData, isLoading: isBoardLoading } = useGetBoardList({ sort: 'like' });
+  const { data: latestBoardData } = useGetBoardList({ limit: 99 });
 
   const sliderRef = useRef<HTMLDivElement>(null);
+  const searchRef = useRef<HTMLInputElement | null>(null);
   const [isFocused, setIsFocused] = useState(false);
+  const [keyword, setKeyword] = useState('');
+
+  const handleChange = () => {
+    if (searchRef.current !== null) {
+      setKeyword(searchRef.current.value);
+    }
+  };
+
+  const filteredBoard =
+    latestBoardData?.filter(({ name }: BoardResults) => name.toLowerCase().includes(keyword.toLowerCase())) ?? [];
+
+  const isPopularBoardEmpty = popularBoardData?.length === 0;
+  const isLatestBoardEmpty = filteredBoard.length === 0;
 
   const handleFocus = () => setIsFocused(true);
   const handleBlur = () => setIsFocused(false);
@@ -52,39 +61,48 @@ const BoardList = () => {
       <main className='m-auto flex max-w-[1120px] flex-col gap-4 py-[100px] pl-5 lg:pl-0'>
         <section className='popular-board'>
           <div className='flex justify-between'>
-            <h2 className='mb-2 text-bold-24 text-yellow-300'>Popular Board</h2>
-            <div className='flex gap-2'>
-              <IconButton
-                iconAlt={left.alt}
-                iconUrl={left.url}
-                variant='stroke'
-                onClick={handlePrevClick}
-                iconSize={24}
-              />
-              <IconButton
-                iconAlt={right.alt}
-                iconUrl={right.url}
-                variant='stroke'
-                onClick={handleNextClick}
-                iconSize={24}
-              />
-            </div>
+            <h2 className='mb-2 text-bold-24 text-yellow-200'>🎉 Popular Board</h2>
+            {popularBoardData?.length > 4 && (
+              <div className='flex gap-2'>
+                <IconButton
+                  iconAlt={left.alt}
+                  iconUrl={left.url}
+                  variant='stroke'
+                  onClick={handlePrevClick}
+                  iconSize={24}
+                />
+                <IconButton
+                  iconAlt={right.alt}
+                  iconUrl={right.url}
+                  variant='stroke'
+                  onClick={handleNextClick}
+                  iconSize={24}
+                />
+              </div>
+            )}
           </div>
 
           <div className='max-w-[1120px] overflow-x-scroll scroll-smooth pt-5 scrollbar-hide' ref={sliderRef}>
-            <ul className='flex flex-row gap-5'>
-              {popularBoardData?.map((board: BoardResults) => (
-                <li key={board.id}>
-                  <BoardCard board={board} />
-                </li>
-              ))}
-            </ul>
+            {isBoardLoading ? (
+              <BoardCardListSkeleton />
+            ) : (
+              <>
+                {isPopularBoardEmpty && <EmptyBoardCards />}
+                <ul className='flex flex-row gap-5'>
+                  {popularBoardData?.map((board: BoardResults) => (
+                    <li key={board.id}>
+                      <BoardCard board={board} />
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
           </div>
         </section>
 
         <section className='latest-board'>
           <div className='mt-10 flex items-center justify-between'>
-            <h2 className='mb-2 text-bold-24 text-green-400'>Latest Board</h2>
+            <h2 className='mb-2 text-bold-24'>Latest Board</h2>
             <div className='flex items-center gap-4'>
               <label htmlFor='search'>
                 <img
@@ -99,20 +117,30 @@ const BoardList = () => {
                 type='text'
                 placeholder='Find Your Mingle Board'
                 className='w-[210px] bg-transparent text-base-18 text-neutral-100 outline-none placeholder:text-neutral-700'
+                onChange={handleChange}
+                value={keyword}
                 onFocus={handleFocus}
                 onBlur={handleBlur}
+                ref={searchRef}
               />
             </div>
           </div>
 
           <div className='max-w-[1120px] overflow-x-scroll scroll-smooth pt-5 scrollbar-hide'>
-            <ul className='grid grid-cols-4 gap-x-5 gap-y-8'>
-              {latestBoardData?.map((board: BoardResults) => (
-                <li key={board.id}>
-                  <BoardCard board={board} />
-                </li>
-              ))}
-            </ul>
+            {isBoardLoading ? (
+              <BoardCardListSkeleton />
+            ) : (
+              <>
+                {isLatestBoardEmpty && <EmptyBoardCard />}
+                <ul className='grid grid-cols-4 gap-x-5 gap-y-8'>
+                  {filteredBoard?.map((board: BoardResults) => (
+                    <li key={board.id}>
+                      <BoardCard board={board} />
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
           </div>
         </section>
       </main>
