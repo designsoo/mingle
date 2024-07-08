6773239d4c8faf1f483d144c376bbbde4506af27
designsoo <kheesoo10@gmail.com>
2024-07-08 22:47:03 +0900

    refactor: 로딩중일 때 보여주는 카드 스켈레톤 개수 변경

--- src/components/pages/DetailBoard/CardListSkeleton.tsx
+++ src/components/pages/DetailBoard/CardListSkeleton.tsx
@@ -1,7 +1,7 @@
 import { CardSkeleton } from 'mingle-ui';
 
 const CardListSkeleton = () => {
-  return Array.from({ length: 9 }).map((_, idx: number) => (
+  return Array.from({ length: 3 }).map((_, idx: number) => (
     <li key={`empty-card-${idx}`}>
       <CardSkeleton />
     </li>
