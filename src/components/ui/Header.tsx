0bb45226fd7fa9376b164f8f5454beeafd424b4d
designsoo <kheesoo10@gmail.com>
2024-07-14 00:21:19 +0900

    refactor: landing 페이지인 경우 header 배경색 변경

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -16,7 +16,9 @@ const Header = ({ isLanding = false }: HeaderProps) => {
   const location = useLocation();
 
   return (
-    <header className='header-background fixed left-0 top-0 z-50 flex h-[64px] w-full items-center justify-between px-5 lg:px-10'>
+    <header
+      className={`fixed left-0 top-0 z-50 flex h-[64px] w-full items-center justify-between px-5 lg:px-10 ${isLanding ? 'backdrop-blur-3xl color-background-opacity-black-1' : 'header-background'}`}
+    >
       <h1>
         <Link to={landing}>
           <img src={url} alt={alt} width={100} height={30} />
