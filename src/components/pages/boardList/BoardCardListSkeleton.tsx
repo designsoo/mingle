08c2c9aa8859f272756a3d858715d1db0f6454ec
designsoo <kheesoo10@gmail.com>
2024-07-14 21:25:36 +0900

    refactor: 디바이스별 보여지는 카드 스켈레톤 레이아웃 변경

--- src/components/pages/boardList/BoardCardListSkeleton.tsx
+++ src/components/pages/boardList/BoardCardListSkeleton.tsx
@@ -2,7 +2,7 @@ import { BoardCardSkeleton } from 'mingle-ui';
 
 const BoardCardListSkeleton = () => {
   return (
-    <ul className='flex flex-row gap-5'>
+    <ul className='grid grid-cols-1 gap-x-5 gap-y-8 md:grid-cols-2 lg:grid-cols-4'>
       {Array.from({ length: 4 }).map((_, index) => (
         <li key={`card-skeleton-${index}`}>
           <BoardCardSkeleton />
