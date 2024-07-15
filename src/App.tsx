e36ac90654d05ae698c5d13d30e9798516015ee2
designsoo <kheesoo10@gmail.com>
2024-07-16 04:10:39 +0900

    feat: open graph 관련 이미지, 타이틀, 트위터 내용 추가

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,21 @@ function App() {
       <Helmet>
         <meta name='description' content={META_DATA.description} />
         <meta name='keywords' content={META_DATA.keywords} />
-        <meta property='og:site_name' content={META_DATA.siteName} />
-        <meta property='og:locale' content='ko_KR' />
+
         <meta property='og:type' content='website' />
+        <meta property='og:site_name' content={META_DATA.siteName} />
+        <meta property='og:title' content={META_DATA.title} />
+        <meta property='og:description' content={META_DATA.description} />
+        <meta property='og:locale' content='ko_KR' />
+        <meta property='og:image' content={META_DATA.ogImage} />
+        <meta property='og:image:type' content='image/png' />
+        <meta property='og:image:width' content={META_DATA.ogImageWidth} />
+        <meta property='og:image:height' content={META_DATA.ogImageHeight} />
+
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:title' content={META_DATA.title} />
+        <meta property='twitter:description' content={META_DATA.description} />
+        <meta property='twitter:image' content={META_DATA.twitterImage} />
       </Helmet>
       <QueryClientProvider client={queryClient}>
         <Router />
