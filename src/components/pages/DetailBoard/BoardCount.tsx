f0e416fbeae921a42e6c7bc2bd44c22c14294a60
designsoo <kheesoo10@gmail.com>
2024-07-16 01:31:49 +0900

    refactor: contrast ratio 개선하기 위해 텍스트 색상 변경

--- src/components/pages/DetailBoard/BoardCount.tsx
+++ src/components/pages/DetailBoard/BoardCount.tsx
@@ -7,15 +7,15 @@ const BoardCount = ({ paperCount, reactionCount }: BoardCountProps) => {
   return (
     <ul className='board-count flex w-full gap-6'>
       <li className='flex items-center gap-2'>
-        <span className='text-base-14 text-neutral-500'>Paper</span>
+        <span className='text-base-14 text-neutral-400'>Paper</span>
         <span className='text-bold-18'>{paperCount}</span>
       </li>
       <li className='flex items-center gap-2'>
-        <span className='text-base-14 text-neutral-500'>Members</span>
+        <span className='text-base-14 text-neutral-400'>Members</span>
         <span className='text-bold-18'>{paperCount}</span>
       </li>
       <li className='flex items-center gap-2'>
-        <span className='text-base-14 text-neutral-500'>Emotions</span>
+        <span className='text-base-14 text-neutral-400'>Emotions</span>
         <span className='text-bold-18'>{reactionCount}</span>
       </li>
     </ul>
