8818fb28a1158bdb75ecf32421af78bceb4ef535
designsoo <kheesoo10@gmail.com>
2024-07-16 04:09:51 +0900

    refactor: 카드 리스트 레이아웃을 flex에서 grid로 변경

--- src/components/pages/boardList/EmptyBoardCards.tsx
+++ src/components/pages/boardList/EmptyBoardCards.tsx
@@ -2,7 +2,7 @@ import { EmptyBoardCard } from 'mingle-ui';
 
 const EmptyBoardCards = () => {
   return (
-    <ul className='flex flex-row gap-5'>
+    <ul className='grid grid-cols-1 gap-x-5 gap-y-8 md:grid-cols-2 lg:grid-cols-4'>
       {Array.from({ length: 4 }).map((_, index) => (
         <li key={`empty-board-cards-${index}`}>
           <EmptyBoardCard />
