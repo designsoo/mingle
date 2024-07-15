f6c54884e78e03c3e5468204e46bfe952227a803
designsoo <kheesoo10@gmail.com>
2024-07-16 01:34:18 +0900

    refactor: 카드 스켈레톤에 isPopular prop 추가

--- src/components/pages/boardList/BoardCardListSkeleton.tsx
+++ src/components/pages/boardList/BoardCardListSkeleton.tsx
@@ -1,11 +1,13 @@
 import { BoardCardSkeleton } from 'mingle-ui';
 
-const BoardCardListSkeleton = () => {
+const BoardCardListSkeleton = ({ isPopular = false }: { isPopular?: boolean }) => {
   return (
-    <ul className='grid grid-cols-1 gap-x-5 gap-y-8 md:grid-cols-2 lg:grid-cols-4'>
+    <ul
+      className={isPopular ? 'flex flex-row gap-5' : 'grid grid-cols-1 gap-x-5 gap-y-8 md:grid-cols-2 lg:grid-cols-4'}
+    >
       {Array.from({ length: 4 }).map((_, index) => (
         <li key={`card-skeleton-${index}`}>
-          <BoardCardSkeleton cardWidth='265px' />
+          <BoardCardSkeleton cardWidth={isPopular ? '256px' : '100%'} />
         </li>
       ))}
     </ul>
