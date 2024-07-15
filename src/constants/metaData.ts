ccc10c88e1ec4ecbe6bfa35c09bd45a5dfaa262a
designsoo <kheesoo10@gmail.com>
2024-07-15 23:48:58 +0900

    feat: 공통으로 적용되는 메타 데이터 상수화

---
+++ src/constants/metaData.ts
@@ -0,0 +1,9 @@
+export const META_DATA = {
+  siteName: 'Mingle',
+  title: 'Mingle | Make Every Moment Memorable',
+  description:
+    'Perfect for birthdays, graduations, thank you notes, and more, our platform helps you convey your emotions and share joyous moments effortlessly',
+  keywords: 'rolling paper, mingle, friendly',
+  ogImageWidth: '800',
+  ogImageHeight: '400',
+};
