a566b5dc2b7f248d6566cfea03b5b0e944e9b213
designsoo <kheesoo10@gmail.com>
2024-07-16 04:07:49 +0900

    feat: 메타 데이터 상수파일에 ogImage, twitterImage URL 추가

--- src/constants/metaData.ts
+++ src/constants/metaData.ts
@@ -1,3 +1,5 @@
+import { CLOUDFLARE_URL } from '@/constants';
+
 export const META_DATA = {
   siteName: 'Mingle',
   title: 'Mingle | Make Every Moment Memorable',
@@ -6,4 +8,6 @@ export const META_DATA = {
   keywords: 'rolling paper, mingle, friendly',
   ogImageWidth: '800',
   ogImageHeight: '400',
+  ogImage: `${CLOUDFLARE_URL}/2e2b588d-275e-4725-bd1f-e23693aea400/width=800,height=400`,
+  twitterImage: `${CLOUDFLARE_URL}/62477126-c66f-4808-675e-761ddd42d300/width=1200,height=630`,
 };
