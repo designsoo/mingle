5c44dd1dd07f30194ef773603408110def73deef
designsoo <kheesoo10@gmail.com>
2024-07-16 01:31:10 +0900

    feat: contrast ratio 개선하기 위해 텍스트 색상 변경, 디바이스별 타이틀 크기 변경

--- src/components/pages/landing/BannerContent.tsx
+++ src/components/pages/landing/BannerContent.tsx
@@ -6,8 +6,8 @@ interface BannerContentProps {
 const BannerContent = ({ title, description }: BannerContentProps) => {
   return (
     <div className='text-center lg:max-w-[430px]'>
-      <h3 className='mb-4 text-bold-20 lg:mb-8 lg:text-bold-28'>{title}</h3>
-      <p className='text-base-14 text-neutral-500 lg:!text-base-18'>{description}</p>
+      <h3 className='mb-4 text-bold-20 text-neutral-100 lg:mb-8 lg:text-bold-28'>{title}</h3>
+      <p className='text-base-14 text-neutral-400 lg:!text-base-18'>{description}</p>
     </div>
   );
 };
