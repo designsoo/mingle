0c850cbf7c5f89d82b818ad2ef4e4ae2097147de
designsoo <kheesoo10@gmail.com>
2024-07-15 23:45:36 +0900

    refactor: 카드 스켈레톤 컴포넌트 너비 prop 지정

--- src/components/pages/boardList/BoardCardListSkeleton.tsx
+++ src/components/pages/boardList/BoardCardListSkeleton.tsx
@@ -5,7 +5,7 @@ const BoardCardListSkeleton = () => {
     <ul className='grid grid-cols-1 gap-x-5 gap-y-8 md:grid-cols-2 lg:grid-cols-4'>
       {Array.from({ length: 4 }).map((_, index) => (
         <li key={`card-skeleton-${index}`}>
-          <BoardCardSkeleton />
+          <BoardCardSkeleton cardWidth='265px' />
         </li>
       ))}
     </ul>
