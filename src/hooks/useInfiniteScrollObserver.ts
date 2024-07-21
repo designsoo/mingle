e3285e037495bb72832e83e3f2b12d7138d16ba0
designsoo <kheesoo10@gmail.com>
2024-07-21 17:25:18 +0900

    refactor: 불필요한 리렌더링을 방지하기 위해 useRef 사용, 주석 삭제, 적절한 함수명으로 변경

--- src/hooks/useInfiniteScrollObserver.ts
+++ src/hooks/useInfiniteScrollObserver.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { InfiniteQueryObserverResult } from '@tanstack/react-query';
 
@@ -7,32 +7,34 @@ interface IuseIntersectionObserverProps {
   fetchNextPage: () => Promise<InfiniteQueryObserverResult>;
 }
 
-export const useIntersectionObserver = ({ hasNextPage, fetchNextPage }: IuseIntersectionObserverProps) => {
-  const [trigger, setTrigger] = useState<HTMLDivElement | null>(null);
+const useInfiniteScrollObserver = ({ hasNextPage, fetchNextPage }: IuseIntersectionObserverProps) => {
+  const trigger = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    if (!trigger) return;
-
     const observerCallback: IntersectionObserverCallback = (entries) => {
       entries.forEach((entry) => {
-        //trigger 화면에 관찰되고, 다음페이지가 있다면 다음페이지를 호출
         if (entry.isIntersecting && hasNextPage) {
           fetchNextPage();
         }
       });
     };
 
-    //ointersection observer 인스턴스 생성
     const observer = new IntersectionObserver(observerCallback, {
       threshold: 0.5,
     });
 
-    // 타겟 관찰 시작
-    observer.observe(trigger);
+    if (trigger.current) {
+      observer.observe(trigger.current);
+    }
 
-    // 관찰 멈춤
-    return () => observer.unobserve(trigger);
+    return () => {
+      if (trigger.current) {
+        observer.unobserve(trigger.current);
+      }
+    };
   }, [trigger, hasNextPage, fetchNextPage]);
 
-  return { setTrigger };
+  return trigger;
 };
+
+export default useInfiniteScrollObserver;
