3cb6100c902708308118094c12d94983f125ba3e
designsoo <kheesoo10@gmail.com>
2024-07-26 16:32:35 +0900

    refactor: preloadImage 유틸 함수 수정

--- src/utils/preloadImages.ts
+++ src/utils/preloadImages.ts
@@ -1,5 +1,12 @@
-export const preloadImages = (imageUrl: string, callback: () => void) => {
-  const img = new Image();
-  img.src = imageUrl;
-  img.onload = callback;
+type imageList = {
+  id: string;
+  type: string;
+  value: string;
+};
+
+export const preloadImages = (imageList: imageList[]) => {
+  imageList.forEach((image) => {
+    const img = new Image();
+    img.src = image.value;
+  });
 };
