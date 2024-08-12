f5f397702c88e15f1e028a9088ca9581b4ddff3d
designsoo <kheesoo10@gmail.com>
2024-08-13 04:16:55 +0900

    refactor: board 리스트 페이지에 무한 스크롤 적용, BoardCardList 컴포넌트 적용, SearchInput 컴포넌트 적용

--- src/pages/BoardList/index.tsx
+++ src/pages/BoardList/index.tsx
@@ -1,59 +1,57 @@
 import { useRef, useState } from 'react';
 
-import { EmptyBoardCard, IconButton } from 'mingle-ui';
+import { IconButton } from 'mingle-ui';
 
 import { SVGS } from '@/constants';
 
 import MetaData from '@/components/MetaData';
-import BoardCard from '@/components/pages/boardList/BoardCard';
-import BoardCardListSkeleton from '@/components/pages/boardList/BoardCardListSkeleton';
-import EmptyBoardCards from '@/components/pages/boardList/EmptyBoardCards';
+import BoardCardList from '@/components/pages/boardList/BoardCardList';
+import SearchInput from '@/components/pages/boardList/SearchInput';
 import Header from '@/components/ui/Header';
 import NavigationBar from '@/components/ui/NavigationBar';
-import { BoardResults } from '@/types/recipients';
+import useInfiniteScrollObserver from '@/hooks/useInfiniteScrollObserver';
 
 import { useGetBoardList } from './data-access/useGetBoardList';
+import { useGetBoards } from './data-access/useGetBoards';
 
 const {
   arrow: { left, right },
-  search,
 } = SVGS;
 
-const SCROLL_SLIDER_WIDRH = 1140;
+const SCROLL_SLIDER_WIDTH = 1140;
+const LATEST_BOARD_LIMIT = 8;
 
 const BoardList = () => {
-  const { data: popularBoardData, isLoading: isBoardLoading } = useGetBoardList({ sort: 'like' });
-  const { data: latestBoardData } = useGetBoardList({ limit: 99 });
-
+  const { data: popularBoards, isLoading: isPopularBoardsFetching } = useGetBoardList({ sort: 'like' });
+  const {
+    latestBoardData,
+    fetchNextPage,
+    hasNextPage,
+    isFetching: isLatestBoardsFetching,
+    isFetchingNextPage,
+  } = useGetBoards({
+    limit: LATEST_BOARD_LIMIT,
+  });
+  const infiniteScrollTriggerRef = useInfiniteScrollObserver({ hasNextPage, fetchNextPage });
   const sliderRef = useRef<HTMLDivElement>(null);
-  const searchRef = useRef<HTMLInputElement | null>(null);
-  const [isFocused, setIsFocused] = useState(false);
   const [keyword, setKeyword] = useState('');
 
-  const handleChange = () => {
-    if (searchRef.current !== null) {
-      setKeyword(searchRef.current.value);
-    }
-  };
+  const filteredBoards =
+    latestBoardData?.pages.filter(({ name }: { name: string }) => name.toLowerCase().includes(keyword.toLowerCase())) ??
+    [];
 
-  const filteredBoard =
-    latestBoardData?.filter(({ name }: BoardResults) => name.toLowerCase().includes(keyword.toLowerCase())) ?? [];
-
-  const isPopularBoardEmpty = popularBoardData?.length === 0;
-  const isLatestBoardEmpty = filteredBoard.length === 0;
-
-  const handleFocus = () => setIsFocused(true);
-  const handleBlur = () => setIsFocused(false);
+  const isPopularBoardEmpty = popularBoards?.length === 0;
+  const isLatestBoardEmpty = filteredBoards.length === 0;
 
   const handleNextClick = () => {
     if (sliderRef.current) {
-      sliderRef.current.scrollLeft += SCROLL_SLIDER_WIDRH;
+      sliderRef.current.scrollLeft += SCROLL_SLIDER_WIDTH;
     }
   };
 
   const handlePrevClick = () => {
     if (sliderRef.current) {
-      sliderRef.current.scrollLeft -= SCROLL_SLIDER_WIDRH;
+      sliderRef.current.scrollLeft -= SCROLL_SLIDER_WIDTH;
     }
   };
 
@@ -66,7 +64,7 @@ const BoardList = () => {
           <section className='popular-board'>
             <div className='flex justify-between pr-5'>
               <h2 className='mb-2 text-bold-24 text-yellow-200'>Popular Board</h2>
-              {popularBoardData?.length > 4 && (
+              {popularBoards?.length > 4 && (
                 <div className='hidden gap-2 lg:flex'>
                   <IconButton
                     iconAlt={left.alt}
@@ -87,66 +85,29 @@ const BoardList = () => {
             </div>
 
             <div className='max-w-[1120px] overflow-x-scroll scroll-smooth pt-5 scrollbar-hide' ref={sliderRef}>
-              {isBoardLoading ? (
-                <BoardCardListSkeleton isPopular />
-              ) : (
-                <>
-                  {isPopularBoardEmpty && <EmptyBoardCards />}
-                  <ul className='flex flex-row gap-5'>
-                    {popularBoardData?.map((board: BoardResults) => (
-                      <li key={board.id}>
-                        <BoardCard board={board} cardWidth={'265px'} />
-                      </li>
-                    ))}
-                  </ul>
-                </>
-              )}
+              <BoardCardList
+                boardList={popularBoards}
+                isLoading={isPopularBoardsFetching}
+                isEmpty={isPopularBoardEmpty}
+                isPopular
+              />
             </div>
           </section>
 
           <section className='latest-board pr-5 lg:pr-0'>
             <div className='mt-10 flex flex-col gap-3 md:!flex-row md:items-center md:justify-between'>
               <h2 className='text-bold-24'>Latest Board</h2>
-              <div
-                className={`flex items-center gap-4 ${isFocused && 'border-yellow-300'} base-transition border-b border-transparent pb-2 md:border-none md:pb-0`}
-              >
-                <label htmlFor='search'>
-                  <img
-                    src={isFocused ? search.active.url : search.default.url}
-                    alt={isFocused ? search.active.alt : search.default.alt}
-                    width={24}
-                    height={24}
-                  />
-                </label>
-                <input
-                  id='search'
-                  type='text'
-                  placeholder='Find Your Mingle Board'
-                  className='h-11 w-[210px] bg-transparent text-base-18 text-neutral-100 outline-none placeholder:text-neutral-700'
-                  onChange={handleChange}
-                  value={keyword}
-                  onFocus={handleFocus}
-                  onBlur={handleBlur}
-                  ref={searchRef}
-                />
-              </div>
+              <SearchInput keyword={keyword} onSearchChange={setKeyword} />
             </div>
 
             <div className='max-w-[1120px] overflow-x-scroll scroll-smooth pt-5 scrollbar-hide'>
-              {isBoardLoading ? (
-                <BoardCardListSkeleton />
-              ) : (
-                <>
-                  {isLatestBoardEmpty && <EmptyBoardCard />}
-                  <ul className='grid grid-cols-1 gap-x-5 gap-y-8 md:grid-cols-2 lg:grid-cols-4'>
-                    {filteredBoard?.map((board: BoardResults) => (
-                      <li key={board.id}>
-                        <BoardCard board={board} />
-                      </li>
-                    ))}
-                  </ul>
-                </>
-              )}
+              <BoardCardList
+                boardList={filteredBoards}
+                isLoading={isLatestBoardsFetching}
+                isEmpty={isLatestBoardEmpty}
+                isNextPageLoading={isFetchingNextPage}
+                triggerRef={infiniteScrollTriggerRef}
+              />
             </div>
           </section>
         </main>
