1b2bf2c161b044d3386480613847ec46202991b2
designsoo <kheesoo10@gmail.com>
2024-08-12 12:53:26 +0900

    feat: 카드 생성 폼 스키마 생성

---
+++ src/pages/AddCard/schema/index.tsx
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const addFormSchema = z.object({
+  name: z.string().min(1, 'Name is Required').min(4, 'Please enter at least 4 characters'),
+  password: z
+    .union([
+      z.string().length(4, 'Password must be exactly 4 characters'),
+      z.string().length(0, 'Password must be empty or exactly 4 characters'),
+    ])
+    .optional(),
+  content: z.string().min(1),
+});
