225168e9a43c96a13bd01f890f50d72fab8db994
designsoo <kheesoo10@gmail.com>
2024-08-12 12:50:15 +0900

    feat: 보드 생성 폼 스키마 생성

---
+++ src/pages/CreateBoard/schema/index.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const ceateFormSchema = z.object({
+  name: z.string().min(1, 'Name is Required').min(4, 'Please enter at least 4 characters'),
+  password: z
+    .union([
+      z.string().length(4, 'Password must be exactly 4 characters'),
+      z.string().length(0, 'Password must be empty or exactly 4 characters'),
+    ])
+    .optional(),
+  backgroundColor: z.string(),
+  backgroundImageURL: z.union([z.string(), z.null()]),
+});
