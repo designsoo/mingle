4a05761c0daedeb37a651a4ca79a13e033aeaf07
designsoo <kheesoo10@gmail.com>
2024-08-13 04:15:46 +0900

    refactor: select를 활용하여 데이터 가공 후 반환되도록 변경

--- src/pages/BoardList/data-access/useGetBoardList.ts
+++ src/pages/BoardList/data-access/useGetBoardList.ts
@@ -3,10 +3,11 @@ import { useQuery } from '@tanstack/react-query';
 import { getBoardList } from '@/api/queryFunctions';
 import { BoardListParams } from '@/types/recipients';
 
-export const useGetBoardList = ({ sort, limit }: BoardListParams) => {
+export const useGetBoardList = ({ limit, sort }: BoardListParams) => {
   const { data, isLoading } = useQuery({
-    queryKey: ['recipients', sort, limit],
-    queryFn: () => getBoardList({ sort, limit }),
+    queryKey: ['recipients', limit, sort],
+    queryFn: () => getBoardList({ limit, sort }),
+    select: (data) => data.results,
   });
 
   return { data, isLoading };
